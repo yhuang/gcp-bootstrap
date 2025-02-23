a590dbeb7790d162ce71048b708b5c5b1e8ec4a9
Jimmy Huang <jimmy.huang@duragility.com>
2025-02-23 00:23:55 -0800

    output some key values

--- output.tf
+++ output.tf
@@ -21,3 +21,15 @@ output "tfc_gcp_oidc_identity_provider_name" {
 output "tfc_gcp_workload_identity_principal_set" {
   value = local.tfc_gcp_workload_identity_principal_set
 }
+
+output "terraform_admin_gcp_project_id" {
+  value = var.terraform_admin_gcp_project_id
+}
+
+output "gcp_folder_id" {
+  value = var.gcp_folder_id
+}
+
+output "gcp_billing_account_id" {
+  value = var.gcp_billing_account_id
+}
